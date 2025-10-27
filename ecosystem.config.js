ef7209a91d935b4d1f05cb79cbfa0eb32d1734e3
nico <nico@PCEPROD0.DFARUNIADM.LOCAL>
2025-10-27 16:56:57 -0500

    added ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  apps: [
+    {
+      name: 'GSS Front',
+      exec_mode: 'cluster',
+      instances: '1', // Or a number of instances
+      script: 'node_modules/next/dist/bin/next',
+      args: 'start',
+      cron_restart: '2 0 * * *',
+      env_local: {
+        APP_ENV: 'local', // APP_ENV=local
+      },
+      env_development: {
+        APP_ENV: 'dev', // APP_ENV=dev
+      },
+      env_production: {
+        APP_ENV: 'prod', // APP_ENV=prod
+      },
+    },
+  ],
+};
