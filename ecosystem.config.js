54e0bf21796e92140d6119a669e920aad1a57ecf
nico <nico@PCEPROD0.DFARUNIADM.LOCAL>
2025-11-07 13:13:06 -0500

    changed ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,14 +7,8 @@ module.exports = {
       script: 'node_modules/next/dist/bin/next',
       args: 'start',
       cron_restart: '2 0 * * *',
-      env_local: {
-        APP_ENV: 'local', // APP_ENV=local
-      },
-      env_development: {
-        APP_ENV: 'dev', // APP_ENV=dev
-      },
-      env_production: {
-        APP_ENV: 'prod', // APP_ENV=prod
+      env: {
+        PORT: 3003, // Custom port for this instance
       },
     },
   ],
