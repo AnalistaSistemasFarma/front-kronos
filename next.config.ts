1c227c4cbfbe3992247307454409ad48d8f983cd
NicolasRojas1927 <nalexanderrojas@ucompensar.edu.co>
2025-11-10 15:09:28 -0500

    email for resolution case (helpdesk)

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    API_EMAIL: process.env.API_EMAIL,
+  },
 };
 
 export default nextConfig;
